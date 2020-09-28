2cc88d6b3e16434f0cebb4d5ab1acba477c92ef0
choibonggeun <dhy00085@naver.com>
2020-09-28 17:42:31 +0900

    greedy1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Algorithm_practice
+프로그래머스 코딩테스트 연습 문제 풀이
+
+출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
---
+++ greedy/1.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    answer = 0
+    setlost = set(lost)-set(reserve)
+    setreserve= set(reserve)- set(lost)
+    for i in setreserve:
+        if i-1 in setlost:
+            setlost.remove(i-1)
+        elif i+1 in setlost:
+            setlost.remove(i+1)
+    answer = n - len(setlost)
+    return answer
